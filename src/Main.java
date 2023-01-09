a8045f3774cfa95f21db078ff25460d829a48404
Musa <79163385+musayuksel@users.noreply.github.com>
2023-01-09 12:59:32 +0000

    Primitive types sizes

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,17 @@
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
         String myName = "Musa";
-        int myNumber = 5;
+        int myNumber = 5;// 4 byte
         System.out.println("Hello, my name is " + myName +  " and my lucky number is: " + myNumber);
-        byte smallNumber = 127;
-        long veryBigNum = 3_000_000_000L;//don't forget the add L
-        float smallDecimal = 3.14F;//don't' forget the F
-        double veryBigDecimal = 3.14;
-        char oneChar= 'a';//single quote
-        boolean isTrue = false;
+        byte smallNumber = 127;//1 byte
+        short middeNumber = 32_000;// 2 byte
+        long veryBigNum = 3_000_000_000L;//don't forget the add L- 8 byte
+        float smallDecimal = 3.14F;//don't' forget the F - 4 byte
+        double veryBigDecimal = 3.14; // 8 byte
+        char oneChar= 'a';//single quote // 2 byte
+        boolean isTrue = false; // 1 byte
 
     }
 }
