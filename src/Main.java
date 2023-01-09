fb727dc7eca3ecb32ae1aed11854ce9283de5743
Musa <79163385+musayuksel@users.noreply.github.com>
2023-01-09 12:50:34 +0000

    Primitive types

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,12 @@ public class Main {
         String myName = "Musa";
         int myNumber = 5;
         System.out.println("Hello, my name is " + myName +  " and my lucky number is: " + myNumber);
+        byte smallNumber = 127;
+        long veryBigNum = 3_000_000_000L;//don't forget the add L
+        float smallDecimal = 3.14F;//don't' forget the F
+        double veryBigDecimal = 3.14;
+        char oneChar= 'a';//single quote
+        boolean isTrue = false;
+
     }
 }
