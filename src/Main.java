83c8bd7c3613e6afdbf18238820fcadbfeccc466
Musa <79163385+musayuksel@users.noreply.github.com>
2023-01-09 12:14:44 +0000

    Add hello world

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String myName = "Musa";
+        int myNumber = 5;
+        System.out.println("Hello, my name is " + myName +  " and my lucky number is: " + myNumber);
     }
 }
