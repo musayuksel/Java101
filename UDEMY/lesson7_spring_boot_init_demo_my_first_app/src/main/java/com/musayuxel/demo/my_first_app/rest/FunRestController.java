39969d5ec21c4c0dc0366932a271ef4b6acb996c
Musa <79163385+musayuksel@users.noreply.github.com>
2023-05-24 21:25:33 +0100

    add params for endpoint

--- UDEMY/lesson7_spring_boot_init_demo_my_first_app/src/main/java/com/musayuxel/demo/my_first_app/rest/FunRestController.java
+++ UDEMY/lesson7_spring_boot_init_demo_my_first_app/src/main/java/com/musayuxel/demo/my_first_app/rest/FunRestController.java
@@ -1,6 +1,7 @@
 package com.musayuxel.demo.my_first_app.rest;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +10,11 @@ public class FunRestController {
     @GetMapping("/")
     public String sayHello(){
         return "Hello World";
+    }
+
+//    PARAMS: http://localhost:8080/hello?name=Musa
+    @GetMapping("/names")
+    public String sayHelloToName(@RequestParam String name){
+        return "Hello " + name;
     }
 }
--- UDEMY/lesson7_spring_boot_init_demo_my_first_app/src/test/java/com/musayuxel/demo/my_first_app/MyFirstAppApplicationTests.java
+++ UDEMY/lesson7_spring_boot_init_demo_my_first_app/src/test/java/com/musayuxel/demo/my_first_app/MyFirstAppApplicationTests.java
@@ -9,5 +9,4 @@ class MyFirstAppApplicationTests {
 	@Test
 	void contextLoads() {
 	}
-
 }
